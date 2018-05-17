d3a1b17625530af245e2b38f0efc379cf864043c
john o'keeffe <jokeeffe09@gmail.com>
2018-05-17 13:03:16 +0100

    Add heroku-postres DB url

--- app/models/index.js
+++ app/models/index.js
@@ -26,12 +26,21 @@ const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, pr
     idle: 10000
   }
 }) */
-
-const sequelize = new Sequelize(config.pg.name, config.pg.user, config.pg.password, {
-  host: config.pg.host,
-  dialect: config.pg.dialect,
-  pool: config.pg.pool
-})
+if (process.env.DATABASE_URL) {
+  const sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect:  'postgres',
+    protocol: 'postgres',
+    port:     match[4],
+    host:     match[3],
+    logging:  true //false
+  })
+} else {
+  const sequelize = new Sequelize(config.pg.name, config.pg.user, config.pg.password, {
+    host: config.pg.host,
+    dialect: config.pg.dialect,
+    pool: config.pg.pool
+  })
+}
 
 let db = {}
 
