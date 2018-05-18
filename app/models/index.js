10aa6a233e65e2da93c4ec72eae5304146eb9db8
john o'keeffe <jokeeffe09@gmail.com>
2018-05-18 09:45:39 +0100

    Change sequelize options object

--- app/models/index.js
+++ app/models/index.js
@@ -30,9 +30,12 @@ if (process.env.DATABASE_URL) {
   const sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialect:  'postgres',
     protocol: 'postgres',
-    port:     match[4],
-    host:     match[3],
-    logging:  true //false
+    // port:     match[4],
+    // host:     match[3],
+    // logging:  true //false
+    dialectOptions: {
+      ssl: true
+    }
   })
 } else {
   const sequelize = new Sequelize(config.pg.name, config.pg.user, config.pg.password, {
